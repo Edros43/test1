1c91d057b7d9ed47ba2f0d786cc7b76b84a03b67
Edros43 <ederaldoss@yahoo.com.br>
2015-11-27 19:17:21 -0200

    update

--- variavel.php
+++ variavel.php
@@ -1,5 +1,18 @@
 <?php
 $nome = "edros";
 echo $nome;
+echo '<br>';
+
+#variavel obscura
+$_13;
+
+#(Usuario) é diferente de (usuario)
+
+$Usuario = 'USER1';
+$usuario = 'user1';
+
+echo $Usuario;
+echo '<br>';
+echo $usuario;
 
 ?>
