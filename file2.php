f7fa56a65cc5afb6a09f8600aceed49d5662738b
Edros43 <ederaldoss@yahoo.com.br>
2015-11-27 00:00:28 -0200

    commit inicial

---
+++ file2.php
@@ -0,0 +1,18 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
+<html>
+<head>
+<title></title>
+<meta name="" content="">
+<meta http-equiv="content-type" content="text/html; charset=utf-8">
+</head>
+<body>
+<h1> Teste Header H1</h1>
+
+<h6> Teste header H6</h6>
+
+<?php 
+echo "oi";
+?>
+
+</body>
+</html>
