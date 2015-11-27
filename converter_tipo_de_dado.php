7fcaaa8301730099dec4f817a707297ff89f7cd0
Edros43 <ederaldoss@yahoo.com.br>
2015-11-27 19:57:03 -0200

    up file converter_tipo_de dados

---
+++ converter_tipo_de_dado.php
@@ -0,0 +1,17 @@
+<?php
+
+
+//forçar converção de tipos de dados
+$variavel = 'Jonas Dias';
+echo (int)$variavel
+
+/* Tipos de dados
+(int)
+(bool)
+(float)
+(string)
+(array())
+(object)
+*/
+
+
--- variavel.php
+++ variavel.php
@@ -2,8 +2,9 @@
 $nome = "edros";
 echo $nome;
 echo '<br>';
+echo '<br>';
 
-#variavel obscura
+#variavel obscura. A variavel deve se auto explicar.
 $_13;
 
 #(Usuario) é diferente de (usuario)
@@ -13,6 +14,56 @@ $usuario = 'user1';
 
 echo $Usuario;
 echo '<br>';
+echo '<br>';
 echo $usuario;
+echo '<br>';
+echo '<br>';
 
-?>
+
+# tipos de variaveis
+
+// números inteiros - integer
+$variavel = 13;
+echo gettype($variavel);
+echo '<br>';
+echo is_integer($variavel);
+echo '<br>';
+
+//números de ponto flutuante - float
+$variavel = 12.4564;
+
+//strings (textos)
+$variavel = 'Jonas Dias';
+$variavel = "Jonas Dias1";
+echo is_string($variavel);
+
+//boleanos - bool
+$variavel = true;
+$variavel = false;
+echo is_string($variavel);
+echo '<br>';
+
+// arrays
+$variavel = array(1, 2, 3, 4, 10.245, 'Jonas', true);
+echo is_array($variavel);
+echo '<br>';
+
+//objetos
+//$variavel = new objeto();
+
+// recursos
+//$variavel = mysql_connect('localhost', 'root');
+
+//nulo
+$variavel = null;
+echo is_null($variavel);
+echo '<br>';
+
+
+// identifica o tipo de variavel
+echo gettype($variavel);
+
+//retorna valor 1 ou 0 indicando se a variavel é do tipo indicado
+echo is_integer($variavel);
+
+
